e2f3884217ef2d532c9178dcd3793ce014d6a7be
Žan Rode <zanr.comedy@gmail.com>
2022-02-15 18:42:56 +0100

    Setup webs. under constr.

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="center">
+      <h1>
+        WEBSITE UNDER CONSTRUCTION
+      </h1>
+    </div>
+  );
+}
+
+export default App;
