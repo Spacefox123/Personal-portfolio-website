5067b194c079cca0a378f5524fe3e02308b10562
Žan Rode <zanr.comedy@gmail.com>
2022-02-15 19:15:54 +0100

    Setup basic app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { PortfolioProvider } from '../context';
+import {
+  headData,
+  socialLinks,
+  skills,
+  contactData
+} from '../data';
 import "./App.scss";
 
+
 function App() {
+  const [headData, setHeaddata] = useState({})
+  const [socialLinks, setSociallinks] = useState([])
+  const [skills, setSkills] = useState([])
+  const [contactData, setContactdata] = useState({})
+
+  useEffect(() => {
+    setHeaddata({...headData});
+    setSociallinks({...socialLinks});
+    setSkills({...skills});
+    setContactdata({...contactData});
+  }, [])
+
   return (
-    <div className="center">
-      <h1>
-        WEBSITE UNDER CONSTRUCTION
-      </h1>
-    </div>
+    <PortfolioProvider value={{headData, socialLinks, skills, contactData}}>
+      <h1>Henlo</h1>
+    </PortfolioProvider>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,51 @@
+import { nanoid } from 'nanoid';
+
+export const headData = {
+    title: 'Žan Rode', // e.g: 'Name | Developer'
+    lang: 'si', // e.g: en, es, fr, jp
+    description: 'Osebni portfolio', // e.g: Welcome to my website
+  };
+
+export const socialLinks = [
+    {
+        id: nanoid(),
+        name: 'facebook',
+        url: 'https://www.facebook.com/zan.rode/',
+      },
+      {
+        id: nanoid(),
+        name: 'instagram',
+        url: 'https://www.instagram.com/zanrcomedy/',
+      },
+      {
+        id: nanoid(),
+        name: 'linkedin',
+        url: 'https://www.linkedin.com/in/%C5%BEan-rode-09053318a/',
+      },
+      {
+        id: nanoid(),
+        name: 'github',
+        url: 'https://github.com/Spacefox123',
+      },]
+
+export const skills = [
+    'JavaScript',
+    'Python',
+    'CSS',
+    'SASS',
+    'ReactJS',
+    'JSX',
+    'GIT',
+    'Adobe Photoshop',
+    'Adobe Illustrator',
+    'Adobe Premiere Pro',
+    'Adobe After Effects'
+]
+
+export const contactData = {
+cta: '031 892 547',
+goodbyetxt:
+    'Do you want to contact me?',
+btn: 'zanrode@gmail.com',
+email: 'zanrode@gmail.com',
+};
